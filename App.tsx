c02244b446dfcc7e7f60d0c318ca521821315940
Muhammad Odilhonov <ikromifortik@gmail.com>
2024-05-17 19:10:57 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-import AppNavigator from './src/navigators';
-import {NavigationContainer} from '@react-navigation/native';
-import NavigationService from './src/navigators/NavigationService';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistor, store} from './src/store';
-import {Provider} from 'react-redux';
-import {I18nextProvider} from 'react-i18next';
-import i18n from './src/i18n';
-import 'react-native-gesture-handler';
+// App.js
 
-function App(): JSX.Element {
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainTabNavigator from './pages/Mein/MainTabNavigator';
+import SplashScreen from './pages/SplashScreen/SplashScreen';
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
   return (
-    <I18nextProvider i18n={i18n()}>
-      <Provider store={store}>
-        <StatusBar
-          backgroundColor="#088395"
-          translucent
-          barStyle="light-content"
-        />
-        <PersistGate loading={null} persistor={persistor}>
-          <NavigationContainer ref={NavigationService._navigator}>
-            <AppNavigator />
-          </NavigationContainer>
-        </PersistGate>
-      </Provider>
-    </I18nextProvider>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Splasha" component={SplashScreen} />
+          <Stack.Screen name="Main" component={MainTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default App;
