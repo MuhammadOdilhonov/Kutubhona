cf9fc11097623ac69df4a5ac12e5ef8f0885662e
rahmoniy <islomrahmoniy@gmail.com>
2024-05-27 19:55:16 +0500

    .

--- ios/SOFF_marketplaceTests/bis_salesTests.m
+++ ios/SOFF_marketplaceTests/bis_salesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SOFF_marketplaceTests : XCTestCase
+@interface KutubxonaTests : XCTestCase
 
 @end
 
-@implementation SOFF_marketplaceTests
+@implementation KutubxonaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
